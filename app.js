afc582fa20e9b09fdae055d38dfb59418a8e8838
meayazz <140396169+meayazz@users.noreply.github.com>
2025-12-21 12:22:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,110 +1,68 @@
-/***********************
- TEMEL FERAİZ ÇEKİRDEĞİ
- (SİLİNMEDİ)
-************************/
+function hesapla(){
+const mal=+document.getElementById("mal").value;
+if(mal<=0){alert("Mal giriniz");return}
 
-function hesapla() {
-  const es = +document.getElementById("es").value;
-  const ogul = +document.getElementById("ogul").value;
-  const kiz = +document.getElementById("kiz").value;
-  const baba = +document.getElementById("baba").value;
-  const anne = +document.getElementById("anne").value;
+let kalan=1;
+let sonuc=[];
 
-  if ([es, ogul, kiz, baba, anne].some(v => v < 0)) {
-    alert("Negatif değer olamaz");
-    return;
-  }
+const es=+esEl.value,ogul=+ogulEl.value,kiz=+kizEl.value;
+const baba=+babaEl.value,anne=+anneEl.value;
+const dede=+dedeEl.value,kardes=+kardesEl.value;
 
-  let kalan = 1;
-  let sonuc = [];
-
-  // EŞ
-  if (es > 0) {
-    let pay = (ogul + kiz) > 0 ? 1/8 : 1/4;
-    sonuc.push({ad:"Eş", pay, tur:"Hissedar"});
-    kalan -= pay;
-  }
-
-  // ANNE
-  if (anne > 0) {
-    let pay = (ogul + kiz) > 0 ? 1/6 : 1/3;
-    sonuc.push({ad:"Anne", pay, tur:"Hissedar"});
-    kalan -= pay;
-  }
-
-  // BABA
-  if (baba > 0 && (ogul + kiz) > 0) {
-    sonuc.push({ad:"Baba", pay:1/6, tur:"Hissedar"});
-    kalan -= 1/6;
-  }
-
-  // ASABE (ÇOCUKLAR)
-  if (ogul + kiz > 0) {
-    let birim = kalan / (ogul*2 + kiz);
-    if (ogul > 0) sonuc.push({ad:"Oğul", pay:birim*2, tur:"Asabe"});
-    if (kiz > 0) sonuc.push({ad:"Kız", pay:birim, tur:"Asabe"});
-    kalan = 0;
-  }
-
-  detayliTabloYaz(sonuc, kalan);
+if(es>0){
+let p=(ogul+kiz)>0?1/8:1/4;
+sonuc.push({ad:"Eş",pay:p});
+kalan-=p;
 }
 
-/*************************
- YENİ EK: AVl / REDD ALT YAPI
-**************************/
-
-function avlKontrol(sonuc) {
-  let toplam = sonuc.reduce((t,s)=>t+s.pay,0);
-  if (toplam > 1) {
-    sonuc.forEach(s => s.pay = s.pay / toplam);
-    return "Avl uygulandı";
-  }
-  return null;
+if(anne>0){
+let p=(ogul+kiz)>0?1/6:1/3;
+sonuc.push({ad:"Anne",pay:p});
+kalan-=p;
 }
 
-function reddUygula(sonuc, kalan) {
-  if (kalan <= 0) return;
-
-  let hissedarlar = sonuc.filter(s => s.tur === "Hissedar");
-  let toplamPay = hissedarlar.reduce((t,s)=>t+s.pay,0);
-
-  hissedarlar.forEach(s => {
-    s.pay += kalan * (s.pay / toplamPay);
-  });
+if(baba>0){
+if(ogul+kiz>0){sonuc.push({ad:"Baba",pay:1/6});kalan-=1/6}
+else{sonuc.push({ad:"Baba",pay:kalan});kalan=0}
 }
 
-/*************************
- YENİ EK: DETAYLI TABLO
-**************************/
-
-function detayliTabloYaz(data, kalan) {
-  const tbody = document.querySelector("#sonuc tbody");
-  tbody.innerHTML = "";
-
-  let avlDurum = avlKontrol(data);
-  if (!avlDurum && kalan > 0) reddUygula(data, kalan);
-
-  data.forEach(d => {
-    const tr = document.createElement("tr");
-    tr.innerHTML = `
-      <td>${d.ad}</td>
-      <td>${d.pay.toFixed(4)}</td>
-      <td>${(d.pay*100).toFixed(2)}%</td>
-    `;
-    tbody.appendChild(tr);
-  });
-
-  if (avlDurum) {
-    const tr = document.createElement("tr");
-    tr.innerHTML = `<td colspan="3"><strong>${avlDurum}</strong></td>`;
-    tbody.appendChild(tr);
-  }
+if(baba==0 && dede>0){
+sonuc.push({ad:"Dede",pay:1/6});
+kalan-=1/6;
 }
 
-/*************************
- AÇIKLAMA:
- - hesapla() SİLİNMEDİ
- - tablo sistemi GENİŞLETİLDİ
- - Avl & Redd ayrı fonksiyon
- - İleride kardeş/dede eklemek için hazır
-**************************/
+if(ogul+kiz>0){
+let birim=kalan/(ogul*2+kiz);
+if(ogul>0)sonuc.push({ad:"Oğul",pay:birim*2});
+if(kiz>0)sonuc.push({ad:"Kız",pay:birim});
+kalan=0;
+}
+
+if(kardes>0 && ogul+kiz+baba==0){
+sonuc.push({ad:"Kardeş",pay:kalan});
+kalan=0;
+}
+
+let toplam=sonuc.reduce((t,s)=>t+s.pay,0);
+if(toplam>1){
+sonuc.forEach(s=>s.pay=s.pay/toplam);
+kalan=0;
+}
+
+yaz(sonuc,mal);
+}
+
+function yaz(data,mal){
+const tb=document.querySelector("#sonuc tbody");
+tb.innerHTML="";
+data.forEach(d=>{
+let tr=document.createElement("tr");
+tr.innerHTML=`<td>${d.ad}</td>
+<td>${d.pay.toFixed(4)}</td>
+<td>${(d.pay*100).toFixed(2)}%</td>
+<td>${(d.pay*mal).toFixed(2)}</td>`;
+tb.appendChild(tr);
+});
+}
+
+const esEl=es,ogulEl=ogul,kizEl=kiz,babaEl=baba,anneEl=anne,dedeEl=dede,kardesEl=kardes;
